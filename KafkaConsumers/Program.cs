ec4fbae367f3f2f59e4364ac7b6569de707acc49
Rodrigo Duarte <rodrigod1987@gmail.com>
2023-08-13 22:21:54 -0300

    Inital

---
+++ KafkaConsumers/Program.cs
@@ -0,0 +1,75 @@
+﻿using Confluent.Kafka;
+using Serilog;
+using System.Threading;
+using System;
+
+namespace KafkaConsumers
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            logger.Information("Testando o consumo de mensagens com Kafka");
+
+            if (args.Length != 2)
+            {
+                logger.Error(
+                    "Informe 2 parâmetros: " +
+                    "no primeiro o IP/porta para testes com o Kafka, " +
+                    "no segundo o Topic a ser utilizado no consumo das mensagens...");
+
+                return;
+            }
+
+            string bootstrapServers = args[0];
+            string topicName = args[1];
+
+            logger.Information($"BootstrapServers = {bootstrapServers}");
+            logger.Information($"Topic = {topicName}");
+
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = bootstrapServers,
+                GroupId = $"{topicName}-group-0",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            CancellationTokenSource cts = new();
+            Console.CancelKeyPress += (_, e) =>
+            {
+                e.Cancel = true;
+                cts.Cancel();
+            };
+
+            try
+            {
+                using var consumer = new ConsumerBuilder<Ignore, string>(config).Build();
+                consumer.Subscribe(topicName);
+
+                try
+                {
+                    while (true)
+                    {
+                        var cr = consumer.Consume(cts.Token);
+                        logger.Information(
+                            $"Mensagem lida: {cr.Message.Value}");
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    consumer.Close();
+                    logger.Warning("Cancelada a execução do Consumer...");
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error($"Exceção: {ex.GetType().FullName} | " +
+                             $"Mensagem: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ KafkaProducer/Program.cs
@@ -0,0 +1,66 @@
+﻿using Confluent.Kafka;
+using Serilog;
+using System;
+using System.Threading.Tasks;
+
+namespace KafkaProducer
+{
+    public class Program
+    {
+        static async Task  Main(string[] args)
+        {
+            var logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            logger.Information("Testando o envio de mensagens com Kafka");
+
+            if (args.Length < 3)
+            {
+                logger.Error(
+                    "Informe ao menos 3 parâmetros: " +
+                    "no primeiro o IP/porta para testes com o Kafka, " +
+                    "no segundo o Topic que receberá a mensagem, " +
+                    "já no terceito em diante as mensagens a serem " +
+                    "enviadas a um Topic no Kafka...");
+                return;
+            }
+
+            string bootstrapServers = args[0];
+            string topicName = args[1];
+
+            logger.Information($"BootstrapServers = {bootstrapServers}");
+            logger.Information($"Topic = {topicName}");
+
+            try
+            {
+                var config = new ProducerConfig
+                {
+                    BootstrapServers = bootstrapServers
+                };
+
+                using (var producer = new ProducerBuilder<Null, string>(config).Build())
+                {
+                    for (int i = 2; i < args.Length; i++)
+                    {
+                        var result = await producer.ProduceAsync(
+                            topicName,
+                            new Message<Null, string>
+                            { Value = args[i] });
+
+                        logger.Information(
+                            $"Mensagem: {args[i]} | " +
+                            $"Status: {result.Status}");
+                    }
+                }
+
+                logger.Information("Concluído o envio de mensagens");
+            }
+            catch (Exception ex)
+            {
+                logger.Error($"Exceção: {ex.GetType().FullName} | " +
+                             $"Mensagem: {ex.Message}");
+            }
+        }
+    }
+}
